ffffd47c546d7e6600da5e86d9cf640a5685e1be
kosmosr <kosmosr@hotmail.com>
2018-07-03 17:15:29 +0800

    继承SQLAlchemy 结合contextmanager yield 使用db时自动commit rollback

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,21 @@
 @author: zmh
 @time: 2018/6/26 13:43
 """
-from flask_sqlalchemy import SQLAlchemy
+from contextlib import contextmanager
+
+from flask_sqlalchemy import SQLAlchemy as _SQLAlcheymy
+
+
+class SQLAlchemy(_SQLAlcheymy):
+
+    @contextmanager
+    def auto_commit(self):
+        try:
+            yield
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            raise e
+
 
 db = SQLAlchemy()
