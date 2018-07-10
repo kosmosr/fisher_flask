39295863b9c6eade053dff51d2861cc903f83103
kosmosr <kosmosr@hotmail.com>
2018-07-10 14:09:45 +0800

    编码添加愿望 pip换豆瓣源

--- app/web/wish.py
+++ app/web/wish.py
@@ -1,3 +1,8 @@
+from flask import flash, redirect, url_for
+from flask_login import login_required, current_user
+
+from app import db
+from app.models.wish import Wish
 from . import web
 
 __author__ = '七月'
@@ -9,8 +14,17 @@ def my_wish():
 
 
 @web.route('/wish/book/<isbn>')
+@login_required
 def save_to_wish(isbn):
-    pass
+    if current_user.can_save_to_list(isbn):
+        with db.auto_commit():
+            wish = Wish()
+            wish.user_id = current_user.id
+            wish.isbn = isbn
+            db.session.add(wish)
+    else:
+        flash('这本书已添加至你的赠书清单或已存在你的心愿清单，请不要重复添加')
+    return redirect(url_for('web.book_detail', isbn=isbn))
 
 
 @web.route('/satisfy/wish/<int:wid>')
