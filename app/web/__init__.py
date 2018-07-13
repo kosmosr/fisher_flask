fb1ee5dd89421f9ced88a069f3e97d16dd74fed5
kosmosr <kosmosr@hotmail.com>
2018-07-13 15:50:54 +0800

    使用errorhandler捕捉404

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -4,10 +4,16 @@
 @author: zmh
 @time: 2018/5/28 13:53
 """
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import LoginManager
 
 web = Blueprint('web', __name__)
 login_manager = LoginManager()
+
+
+@web.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
 
 from app.web import auth, book, drift, gift, main, wish
