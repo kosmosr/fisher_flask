0e1fb0c3facce33896f3912d4ed0ce35ae83e4f7
kosmosr <kosmosr@hotmail.com>
2018-08-31 11:23:01 +0800

    [U]修改生产配置，库加入gunicorn, gevent, 更新README.md

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 [鱼书](https://coding.imooc.com/class/194.html) 
 
 - **数据库设计**: [showdoc](https://www.showdoc.cc/web/#/124967910301997)
+
+配置统一放在文件夹config下, **DevelopmentConfig**为开发环境配置，**ProductionConfig**为生产环境配置，请根据自己的实际情况修改。
+生产环境的FRONT_URL请修改为能访问的服务器ip，生产环境端口不需要配置端口因为直接有gunicorn配置端口
+
--- config/config.py
+++ config/config.py
@@ -4,7 +4,6 @@
 @author: zmh
 @time: 2018/5/29 11:14
 """
-import os
 
 
 class DevelopmentConfig:
@@ -14,7 +13,6 @@ class DevelopmentConfig:
     ENV = 'development'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Zengminghao_1997@10.10.76.1:13306/fisher'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '97ab1951-3548-4b9a-8f74-5310b552335b'
     TRAP_HTTP_EXCEPTIONS = True
 
     # EMAIL
@@ -35,7 +33,7 @@ class DevelopmentConfig:
     # 1 day
     LOGIN_TOKEN_EXPIRE_TIME = 60 * 60 * 24
 
-    # RESET EMAIL URL
+    # FRONT URL
     FRONT_RESET_EMAIL_URL = 'http://localhost:8081'
     FRONT_DRIFT_URL = 'http://localhost:8081/drift'
 
@@ -43,4 +41,31 @@ class DevelopmentConfig:
 class ProductionConfig:
     DEBUG = False
     ENV = 'production'
-    PORT = os.getenv('port', 8000)
+    HOST = 'localhost'
+
+    # ORM
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fisher:Zengminghao_1997@mysql:3306/fisher'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TRAP_HTTP_EXCEPTIONS = True
+
+    # EMAIL
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'zmh376170361@vip.qq.com'
+    MAIL_PASSWORD = 'zhkytoxtvejfcaji'
+    MAIL_DEFAULT_SENDER = 'zmh376170361@vip.qq.com'
+    MAIL_SUBJECT_PREFIX = '[FISHER]'
+
+    # REDIS
+    REDIS_URL = "redis://redis:6379/0"
+
+    # EXPIRES TIME
+    # 3 hours
+    RESET_TOKEN_EXPIRE_TIME = 60 * 60 * 3
+    # 1 day
+    LOGIN_TOKEN_EXPIRE_TIME = 60 * 60 * 24
+
+    # FRONT URL
+    FRONT_RESET_EMAIL_URL = 'http://localhost'
+    FRONT_DRIFT_URL = 'http://localhost/drift'
