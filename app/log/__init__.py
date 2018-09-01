3bd53010d6b0ec04b62d21f73ef739a1aed22dfc
kosmosr <kosmosr@hotmail.com>
2018-09-01 22:50:19 +0800

    [U]配置日志一天一个文件

--- app/log/__init__.py
+++ app/log/__init__.py
@@ -5,8 +5,10 @@
 @time: 2018/8/5 21:32
 """
 import logging
+import os
+from logging.handlers import TimedRotatingFileHandler
 
-logger = logging.getLogger('fisher')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 console_formater = logging.Formatter(
@@ -15,4 +17,12 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 console_handler.setFormatter(console_formater)
 
+file_path = os.path.join(os.getcwd(), 'app', 'log')
+file_path = os.path.join(file_path, 'app.log')
+file_handler = TimedRotatingFileHandler(file_path, when='D')
+file_handler.setLevel(logging.DEBUG)
+file_handler.suffix = "%Y-%m-%d"
+file_handler.setFormatter(console_formater)
+
 logger.addHandler(console_handler)
+logger.addHandler(file_handler)
